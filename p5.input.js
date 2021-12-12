fdcb5792ffa3ce4fffdaff81b34eec2bfb76f23e
BuggemDeveloper <66670928+BuggemDeveloper@users.noreply.github.com>
2021-12-13 07:27:12 +1100

    Update p5.input.js

--- p5.input.js
+++ p5.input.js
@@ -15,25 +15,25 @@ String.prototype.replaceAll = function (stringToFind, stringToReplace) {
 };
 
 function isHeld(_k) {
-  return keysHeld.includes(_k.toUpperCase());
+  return keysHeld.includes(_k);
 }
 document.addEventListener('keydown', function (e) {
-	theKey = e.key.toUpperCase();
-	keysLog+= e.key.toUpperCase();
+	theKey = e.key;
+	keysLog+= e.key;
 	if(isHeld(e.key)) {
 		return;
 	}
 	aKeyPressed = true;
-  keysHeld = keysHeld.replaceAll(e.key.toUpperCase(), "");
+  keysHeld = keysHeld.replaceAll(e.key, "");
 
-  keysHeld += e.key.toUpperCase();
+  keysHeld += e.key;
   // testing 
 	//console.log(keysHeld);
 }
 );
 document.addEventListener('keyup', function (e) {
-  theKey = e.key.toUpperCase();
-  keysHeld = keysHeld.replaceAll(e.key.toUpperCase(), "");
+  theKey = e.key;
+  keysHeld = keysHeld.replaceAll(e.key, "");
 	if(keysHeld.length <= 0) { aKeyPressed = false; }
 	// testing 
 	//console.log(keysHeld);
