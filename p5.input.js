45f6d7ca79c4f44e22414628cb6a2b4e7ce6c0af
Buggem <66670928+Buggem@users.noreply.github.com>
2022-09-22 14:19:28 +1000

    prepare for array changes to keysHeld

--- p5.input.js
+++ p5.input.js
@@ -1,19 +1,22 @@
-window.keysHeld = "";
+window.keysHeld = [];
 window.theKey = "";
 window.keysLog = "";
 window.aKeyPressed = false;
 
-String.prototype.replaceAll = function (stringToFind, stringToReplace) {
+/*String.prototype.replaceAll = function (stringToFind, stringToReplace) {
     if (stringToFind === stringToReplace) return this;
     var temp = this;
     // debug line
     // console.log((stringToFind.length > 0) + " and " + temp.includes(stringToFind))
     temp = (((stringToFind.length > 0) && temp.includes(stringToFind)) ? (temp.split(stringToFind).join(stringToReplace)) : (temp))
     return temp;
-};
+};*/
 
 window.isHeld = function(_k) {
 	return window.keysHeld.includes(_k);
+};
+window.getKeysHeldAsString = function() {
+	return keysHeld.join('');	
 };
 document.addEventListener('keydown', function (e) {
 	window.theKey = e.key;
@@ -22,16 +25,16 @@ document.addEventListener('keydown', function (e) {
 		return;
 	}
 	window.aKeyPressed = true;
-	window.keysHeld = window.keysHeld.replaceAll(e.key, "");
+	window.keysHeld = window.keysHeld.filter(key => (e.key !== key));
 	
-	window.keysHeld += e.key;
+	window.keysHeld.push(e.key);
 	// testing 
 	// console.log(window.keysHeld);
 }
 );
 document.addEventListener('keyup', function (e) {
 	window.theKey = e.key;
-	window.keysHeld = window.keysHeld.replaceAll(e.key, "");
+	window.keysHeld = window.keysHeld.filter(key => (e.key !== key));
 	if(window.keysHeld.length <= 0) { window.aKeyPressed = false; }
 	// testing 
 	//console.log(keysHeld);
